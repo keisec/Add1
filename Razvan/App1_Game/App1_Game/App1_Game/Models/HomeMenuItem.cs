c61ee13a4792ca800ec1290d0de3bcd43b606934
RazvanCos <razvan_costin@ymail.com>
2020-04-13 13:34:58 +0300

    2nd Commit

---
+++ Razvan/App1_Game/App1_Game/App1_Game/Models/HomeMenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1_Game.Models
+{
+    public enum MenuItemType
+    {
+        Browse,
+        About,
+        Game
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
